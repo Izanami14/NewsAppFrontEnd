04ccc9d59da02bd871fd910cc76dc15fc81169e3
Izanami14 <apurv1980@yahoo.co.in>
2020-08-02 18:30:01 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CardView from './components/card'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: [] };
+  }
+
+  callApi() {
+    fetch("http://localhost:9000/test")
+      .then(res => res.text())
+      .then((res) => {
+        console.log(res);
+        this.setState(
+          {
+            apiResponse: JSON.parse(res)
+          }
+        )
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
+
+  componentDidMount() {
+    this.callApi();
+  }
+
+  render() {
+    let middle = [];
+    let outer = [];
+    return(
+      <div className="App">
+        <div className="container">
+        <p>"Here is what it is"</p>
+       </div>
+      </div>
+
+    );
+    for (let index = 0; index < this.state.apiResponse.length; index++) {
+      if (index === 0 || index % 3 === 0) {
+        middle = [];
+        outer.push
+          (
+            <div className="row top-buffer">
+            </div>);
+      }
+      middle.push(
+        <div className="col-sm">
+          <CardView />
+        </div>
+      )
+      outer[outer.length - 1] = <div className="row top-buffer">
+        {middle}
+      </div>
+
+    }
+    return (
+      <div className="App">
+        <div className="container">
+          {outer}
+        </div>
+
+      </div>
+    );
+  }
 }
-
 export default App;
